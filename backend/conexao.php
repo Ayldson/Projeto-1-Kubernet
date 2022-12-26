9c68d1439a7126b506f236eb74f1b583d3efb89d
ayldson <ayldson@gmail.com>
2022-12-26 16:12:59 -0300

    first commit

---
+++ backend/conexao.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "mysql-connerction";
+$username = "root";
+$password = "leleco20";
+$database = "meubanco";
+
+// Criar conexão
+
+
+$link = new mysqli($servername, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+?>
